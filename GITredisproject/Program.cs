340af6136ffe0073b0bf842ba06397a151fa0f32
kunal2912-code <kunal2912prasad@gmail.com>
2024-10-06 13:43:56 +0530

    added

---
+++ GITredisproject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GITredisproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello everyone, this is my project on github!");
+        }
+    }
+}
